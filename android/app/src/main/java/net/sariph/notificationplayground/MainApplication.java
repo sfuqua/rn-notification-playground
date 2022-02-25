cdff972399ba78949929b49a6b021ec852e49c5f
Steven Fuqua <steven@sariph.net>
2022-02-25 10:23:53 -0800

    Fix build

--- android/app/src/main/java/net/sariph/notificationplayground/MainApplication.java
+++ android/app/src/main/java/net/sariph/notificationplayground/MainApplication.java
@@ -2,6 +2,8 @@ package net.sariph.notificationplayground;
 
 import android.app.Application;
 import android.content.Context;
+
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -61,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.notificationplayground.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("net.sariph.notificationplayground.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
